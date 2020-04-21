acc0f6dba13c69797f1484b7afb4cf309032fc7d
magrcruz <50184830+magrcruz@users.noreply.github.com>
2020-04-21 12:25:34 -0500

    Update polybiosCipher.cpp

--- Polybios/src/polybiosCipher.cpp
+++ Polybios/src/polybiosCipher.cpp
@@ -5,11 +5,11 @@ polybiosCipher::polybiosCipher(){
 
 string polybiosCipher::cifrado(string mensaje){
     string cifrado;
-    for (int i=0;i<mensaje.size();i++){
-        int aux=alfabeto.find(mensaje[i]);
-        if(aux>8) aux--;
-        cifrado+=alfabeto.at(aux/5);
-        cifrado+=alfabeto.at(aux%5);
+    for (int i=0;i<mensaje.size();i++){///por cada letra del mensaje
+        int aux=alfabeto.find(mensaje[i]);///busca en la matriz la posicion de la letra
+        if(aux>8) aux--;///Resta 1 ya que I/J comparten posicion
+        cifrado+=alfabeto.at(aux/5);///busca la columna y concatena
+        cifrado+=alfabeto.at(aux%5);///busca la fila y concatena
     }
     return cifrado;
 }
@@ -17,9 +17,10 @@ string polybiosCipher::cifrado(string mensaje){
 string polybiosCipher::descifrado(string mensajeCifrado){
     string mensaje;
     for(int i=0;i<mensajeCifrado.size();i+=2){
+        ///El primer caracter corresponde a la columna y el segundo a la fila
         int aux=alfabeto.find(mensajeCifrado[i])*5+alfabeto.find(mensajeCifrado[i+1]);
-        if(aux>8) aux++;
-        mensaje+=alfabeto.at(aux);
+        if(aux>8) aux++;///evalua si es mayor a 8 por la posicion I/J
+        mensaje+=alfabeto.at(aux);///concatena en el mensaje de salida
     }
     return mensaje;
 }
