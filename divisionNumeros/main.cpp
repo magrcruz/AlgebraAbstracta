56f1f157af66952196e6b5d05ee53bcfc1ef2c93
magrcruz <50184830+magrcruz@users.noreply.github.com>
2020-04-27 11:24:19 -0500

    contenido del main a funcion

--- divisionNumeros/main.cpp
+++ divisionNumeros/main.cpp
@@ -2,21 +2,22 @@
 
 using namespace std;
 
-void divison(int a,int n,int &q,int&r){
-
-
-}
-
-int main()
-{
-    //a = q*n + r
-    int a,q,n,r;
-    bool negativo=0;
-    cin>>a>>n;
+int modulo(int a,int n){//main a funcion para hallar modulo
+    int q,r;
     q=a/n;
     if(q<0) q--;
     r=a-q*n;
     cout<<"q: "<<q<<" r: "<<r<< endl;
     cout<<a<<"="<<q<<"*"<<n<<"+"<<r<<endl;
+    return r;
+}
+
+int main()
+{
+    int a,n;
+    cin>>a>>n;
+    //contenido del main a funcion
+    cout<<"modulo: "<<modulo(a,n);
+    ///Implementacion con ZZ en Practica de Laboratorio Euclides
     return 0;
 }
