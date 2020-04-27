6a0d7a1740677cdec7744473b43fd2fefd75541d
magrcruz <50184830+magrcruz@users.noreply.github.com>
2020-04-27 11:16:24 -0500

    Add files via upload

---
+++ Practica Laboratorio Euclides/include/mcdAlgorithms.h
@@ -0,0 +1,30 @@
+#ifndef MCDALGORITHMS_H
+#define MCDALGORITHMS_H
+
+#include <iostream>
+#include <NTL/ZZ.h>
+
+using namespace std;
+using namespace NTL;
+
+class mcdAlgorithms
+{
+    public:
+        static bool even(ZZ a);
+        static ZZ mod(ZZ a,ZZ b);
+        static ZZ algoritmo1(ZZ a,ZZ b);
+        static ZZ algoritmo2(ZZ a,ZZ b);
+        static ZZ algoritmo3(ZZ a,ZZ b);
+        static ZZ algoritmo4(ZZ a,ZZ b);
+        static ZZ algoritmo5(ZZ a,ZZ b);
+        static ZZ algoritmo6(ZZ a,ZZ b);
+        static ZZ euclidesRecursivo(ZZ a,ZZ b);
+        static ZZ mods(ZZ a,ZZ b){return a-((a/b)*b);}
+        static ZZ valAbs(ZZ a);
+
+    protected:
+
+    private:
+};
+
+#endif // MCDALGORITHMS_H
---
+++ Practica Laboratorio Euclides/src/mcdAlgorithms.cpp
@@ -0,0 +1,89 @@
+#include "mcdAlgorithms.h"
+
+
+bool mcdAlgorithms::even(ZZ a){
+    if(mods(a,ZZ(2))==0) return 1;
+    return 0;
+}
+
+ZZ mcdAlgorithms::valAbs(ZZ a){
+    if (a<0) return (a*-1);
+    return a;
+    }
+ZZ mcdAlgorithms::algoritmo1(ZZ a,ZZ b){
+    ZZ r=mods(a,b);//p2
+   while(r!=0){
+        cout<<"a: "<<a<<" b: "<<b<<" r: "<<r<<endl;
+        a=b;
+        b=r;
+        r=mods(a,b);//p2
+    }
+    return b;
+}
+ZZ mcdAlgorithms::algoritmo2(ZZ a,ZZ b){
+    ZZ r=mods(a,b);//p2
+    while(r!=0){
+        cout<<"a: "<<a<<" b: "<<b<<" r: "<<r<<endl;
+        if(r>b/2){
+            r=b-r;
+            cout<<"r: "<<r<<endl;
+        }
+        a=b;
+        b=r;
+        r=mods(a,b);//p2
+    }
+    return b;
+}
+ZZ mcdAlgorithms::algoritmo3(ZZ a,ZZ b){
+    cout<<"a: "<<a<<" b: "<<b<<endl;
+    if(b==0) return a;
+    return algoritmo3(b,mods(a,b));
+}
+ZZ mcdAlgorithms::algoritmo4(ZZ a,ZZ b){
+    cout<<"a: "<<a<<" b: "<<b<<endl;
+    if(valAbs(b)>valAbs(a)) return algoritmo4(b,a);
+    else if(b==0) return a;
+    else if(even(a)&&even(b)) return 2* algoritmo4(a/2,b/2);
+    else if(even(a)&&!even(b)) return algoritmo4(a/2,b);
+    else if(!even(a)&&even(b)) return algoritmo4(a,b/2);
+    else return algoritmo4((valAbs(a)-valAbs(b))/2,b);
+}
+ZZ mcdAlgorithms::algoritmo5(ZZ x,ZZ y){
+    ZZ g=ZZ(1);
+    while(even(x)&&even(y)){
+        x=x/2;
+        y=y/2;
+        g=2*g;
+        cout<<"x: "<<x<<" y: "<<y<<" g: "<<g<<endl;
+    }
+    while(x!=0){
+        while(even(x)){
+            x/=2;
+            cout<<"x: "<<x<<endl;
+        }
+        while(even(y)){
+            y/=2;
+            cout<<"y: "<<y<<endl;
+        }
+        ZZ t=valAbs((x-y))/2;
+        cout<<"t: "<<t<<endl;
+        if(x>=y) x=t;
+        else y=t;
+        cout<<"x: "<<x<<" y: "<<y<<endl;
+    }
+    return (g*y);
+}
+ZZ mcdAlgorithms::algoritmo6(ZZ a,ZZ b){
+    while(a!=b){
+        cout<<"a: "<<a<<" b: "<<b<<endl;
+        if(a>b) a=a-b;
+        else b=b-a;
+    }
+    return a;
+}
+ZZ mcdAlgorithms::euclidesRecursivo(ZZ a,ZZ b){
+    ZZ r=mods(a,b);//p2
+    cout<<"a: "<<a<<" b: "<<b<<" r: "<<r<<endl;
+    if(r==0) return b;
+    return euclidesRecursivo(b,r);
+}
