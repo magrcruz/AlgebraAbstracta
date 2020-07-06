caa2067a2994096e537f1a1bf657d18478548037
Magr Cruz <magr300@gmail.com>
2020-07-05 22:57:19 -0500

    opt

--- NumerosAleatorios/main.cpp
+++ NumerosAleatorios/main.cpp
@@ -12,7 +12,7 @@ vector<int> fillWithMemoryInfo() {
     string a;
     if (EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded)) {//obtiene los procesos en aProcesses
         cProcesses = cbNeeded / sizeof(DWORD);// Calcula cuantos procesos fueron retornados
-        for (int i = 100; i < cProcesses&& a.size()<10; i++){
+        for (int i = 100; i < cProcesses&& a.size()<15; i++){
             HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, aProcesses[i]);
             if (NULL != hProcess){
                 PROCESS_MEMORY_COUNTERS pmc;
@@ -78,19 +78,19 @@ void print(vector<bool>::iterator begins,vector<bool>::iterator ends){
 vector<bool> DES(int bits=1024){
     vector<bool>k=initialize(fillWithMemoryInfo());
     int nBits=bits/48+1;
-    int PC_1[]={57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,11,3,60,52,44,36,63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,28,20,12,4};
-    int PC_2[]={14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2,41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32};
+    int PC_1[]={56,48,40,32,24,16,8,0,57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,60,52,44,36,28,20,12,4,27,19,11,3};
+    int PC_2[]={13,16,10,23,0,4,2,27,14,5,20,9,22,18,11,3,25,7,15,6,26,19,12,1,40,51,30,36,46,54,29,39,50,44,32,47,43,48,38,55,33,52,45,41,49,35,28,31};
     int rotaciones[]={1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1,1,1,2,2,2,2};
     vector<bool> K;//para guardar todas las k
     vector<bool> c; vector<bool> d;
-    for(int i=0;i<28;i++) c.push_back(k.at(PC_1[i]-1));
-    for(int i=28;i<56;i++) d.push_back(k.at(PC_1[i]-1));
+    for(int i=0;i<28;i++) c.push_back(k.at(PC_1[i]));
+    for(int i=28;i<56;i++) d.push_back(k.at(PC_1[i]));
     for(int i=0;i<nBits;i++){
         izqRotate(c,rotaciones[i]);
         izqRotate(d,rotaciones[i]);
         vector<bool> both(c.begin(),c.end());
         both.insert(both.end(),d.begin(),d.end());
-        for(int i=0;i<48;i++) K.push_back(both.at(PC_2[i]-1));
+        for(int i=0;i<48;i++) K.push_back(both.at(PC_2[i]));
     }
     K.resize(bits);
     K[0]=1; K[bits-1]=1;
@@ -109,6 +109,6 @@ ZZ conversion(vector<bool> vec){
 }
 
 int main(){
-    cout<<conversion(DES(1024));
+    conversion(DES(1024));
     return 0;
 }
