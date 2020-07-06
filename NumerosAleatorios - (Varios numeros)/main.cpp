f094d7e442603a7d4ec4e0ed5453a5a6d6f95a18
Magr Cruz <magr300@gmail.com>
2020-07-06 15:16:28 -0500

    aleatorios optimizacion

--- NumerosAleatorios - (Varios numeros)/main.cpp
+++ NumerosAleatorios - (Varios numeros)/main.cpp
@@ -4,11 +4,12 @@
 #include <psapi.h>
 #include <bitset>
 #include <NTL/ZZ.h>
+#include <string>
 using namespace NTL;
 using namespace std;
 
 int contador=0;
-vector<string> processInfo;
+string processInfo;
 
 void fillWithMemoryInfo() {
     DWORD aProcesses[1024], cbNeeded, cProcesses;
@@ -19,28 +20,28 @@ void fillWithMemoryInfo() {
             if (NULL != hProcess){
                 PROCESS_MEMORY_COUNTERS pmc;
                 if (GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc))) {
-                    processInfo.push_back(to_string(pmc.PageFaultCount)+to_string(pmc.WorkingSetSize)+
+                    processInfo+=(to_string(pmc.PageFaultCount)+to_string(pmc.WorkingSetSize)+
                     to_string(pmc.QuotaPagedPoolUsage)+to_string(pmc.QuotaNonPagedPoolUsage)+to_string(pmc.PeakPagefileUsage));
-                } // a = parametros del Proceso
+                }
             }
             CloseHandle(hProcess);
         }
     }
 }
-
 vector<int> generateSeed(){
-    if(processInfo.size()<=contador){
+    if((processInfo.size()<contador+15)||processInfo.empty()){
+        processInfo.clear();
         fillWithMemoryInfo();
-        contador =0;
+        contador=0;
     }
     vector<int> k;
-    for(int i=0, j=0;i<5;i++,j+=3){
-        int n=stoi(processInfo[contador].substr(j,3));
+    for(int i=0, j=contador;i<5;i++,j+=3){
+        int n=stoi(processInfo.substr(j,3));
         while(n>255) n>>=1;
         while(n<128) n<<=1;
         k.push_back(n);
     }
-    contador++;
+    contador+=15;
     return k;
 }
 int modint(int a,int n){
@@ -49,22 +50,26 @@ int modint(int a,int n){
     return a-(n*q);
 }
 vector<bool> initialize(vector<int> semilla){
-    vector<int> S;//inicializo con el tamano ya que no necesita reubicar el vector en caso sea mayor
-    for(int i=0;i<256;i++) S.push_back(i);
-    vector<int> K;
-    for(int i=0,k=0;i<=51;i++)
-        for(int j=0;j<5;j++,k++)
-            K.push_back(semilla[j]);
-    for(int i=0,f=0;i<256;i++){
-        f=modint(f+S[i]+K[i],256);
-        swap(S[i],S[f]);
-    }
     vector<int> Or;
+    {//permite que S solo pertenezca a este scope
+    vector<int> S;
+    for(int i=0;i<256;i++) S.push_back(i);
+    {//permite que k solo exista en este scope, optimiza memoria
+        vector<int> K;
+        for(int i=0,k=0;i<=51;i++)
+            for(int j=0;j<5;j++,k++)
+                K.push_back(semilla[j]);
+        for(int i=0,f=0;i<256;i++){
+            f=modint(f+S[i]+K[i],256);
+            swap(S[i],S[f]);
+        }
+    }
     for(int i=0,f=0,k=0;k<8;k++){
         i=modint(i+1,256);
         f=modint(f+S.at(i),256);
         swap(S.at(i),S.at(f));
         Or.push_back(S.at(modint(S.at(i)+S.at(f),256)));//t
+    }
     }
     vector<bool> out;
     for(int i=0;i<8;i++){
@@ -80,28 +85,29 @@ void izqRotate(vector<bool> &vec, int times){
     vec.erase(vec.begin(),vec.begin()+times);
 }
 
-int PC_1[]={57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,11,3,60,52,44,36,63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,28,20,12,4};
-int PC_2[]={14,17,11,24,1,5,3,28,15,6,21,10,23,19,12,4,26,8,16,7,27,20,13,2,41,52,31,37,47,55,30,40,51,45,33,48,44,49,39,56,34,53,46,42,50,36,29,32};
+//no son necesarias multiples copias de estos valores
+int PC_1[]={56,48,40,32,24,16,8,0,57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,60,52,44,36,28,20,12,4,27,19,11,3};
+int PC_2[]={13,16,10,23,0,4,2,27,14,5,20,9,22,18,11,3,25,7,15,6,26,19,12,1,40,51,30,36,46,54,29,39,50,44,32,47,43,48,38,55,33,52,45,41,49,35,28,31};
+int rotaciones[]={1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1,1,1,2,2,2,2};
 
 vector<bool> DES(int bits=1024){
+    vector<bool> K;//para guardar todas las k
+    {
     vector<bool>k=initialize(generateSeed());
     int nBits=bits/48+1;
-
-    int rotaciones[]={1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1,1,1,2,2,2,2};
-    vector<bool> K;//para guardar todas las k
     vector<bool> c; vector<bool> d;
-    for(int i=0;i<28;i++) c.push_back(k.at(PC_1[i]-1));
-    for(int i=28;i<56;i++) d.push_back(k.at(PC_1[i]-1));
+    for(int i=0;i<28;i++) c.push_back(k.at(PC_1[i]));
+    for(int i=28;i<56;i++) d.push_back(k.at(PC_1[i]));
     for(int i=0;i<nBits;i++){
         izqRotate(c,rotaciones[i]);
         izqRotate(d,rotaciones[i]);
         vector<bool> both(c.begin(),c.end());
         both.insert(both.end(),d.begin(),d.end());
-        for(int i=0;i<48;i++) K.push_back(both.at(PC_2[i]-1));
+        for(int i=0;i<48;i++) K.push_back(both.at(PC_2[i]));
+    }
     }
     K.resize(bits);
     K[0]=1; K[bits-1]=1;
-    cout<<endl;
     return K;
 }
 
@@ -116,7 +122,8 @@ ZZ conversion(vector<bool> vec){
 }
 
 int main(){
-    for(int i=0;i<10;i++)
-        cout<<conversion(DES(1024));
+    for(int i=0;i<1000;i++)
+        conversion(DES(1024));
     return 0;
 }
+//tiempo de ejecucion para generacion 1000 numeros aleatorios medido sin chronos: 0.7 aproximadamente
